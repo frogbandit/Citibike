d66e83aef0e14316b1e8050e26139a73a4fd0c5f
Kevin <kevinye1113@gmail.com>
2017-04-26 03:47:56 -0400

    Changed app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,6 +9,14 @@ app.config.update(
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template('index.html')
+
+
+@app.route('/CitiBike', methods=['GET', 'POST'])
+def citibike():
+
+
+
     return render_template('index.html')
 
 if __name__ == '__main__':
