64dde21357674c0fc0cc155e8c77cc7c8e1ff8b9
James Xue <james.xue@columbia.edu>
2017-04-26 01:22:44 -0400

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import json
 from flask import Flask, render_template
-from tweet_listener import get_tweets
-from sentiment_analyzer import get_tweet_sentiments
 
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
-    SECRET_KEY='EVERTWEET'
 )
 
 @app.route('/', methods=['GET', 'POST'])
